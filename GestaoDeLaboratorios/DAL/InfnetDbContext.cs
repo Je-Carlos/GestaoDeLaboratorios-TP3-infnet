2ea8bf5a1089f2fec35ea4ba91045dfe57a253f1
Jean Carlos <cpjeanberg@gmail.com>
2024-03-23 14:11:44 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/DAL/InfnetDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GestaoDeLaboratorios.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class InfnetDbContext : DbContext
+{
+    public InfnetDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Computadores> Computadores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+}
